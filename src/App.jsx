dd2a309dbb57fed03d7562846131faaa43955988
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-19 22:02:18 +0200

    created routes and used useAuth

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import './styles.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
