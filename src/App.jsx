3a7455814d824ec4b5178f65fea98e574d8cad3e
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-19 05:25:09 +0200

    created styles scss file and import inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Register from './pages/Register';
+import './styles.scss';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Register />
+    </div>
+  );
 }
 
 export default App;
