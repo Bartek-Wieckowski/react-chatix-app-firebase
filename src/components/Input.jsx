db47268e3068a3e184c4a82b86083271264418a1
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-19 13:48:02 +0200

    created template of input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+import Attach from '../images/paperclip.png';
+import Img from '../images/file-image.png';
+function Input() {
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: 'none' }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
