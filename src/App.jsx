360a86a0226a3efff88ae8ce2cdfcdc85ea2d795
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-19 11:23:45 +0200

    used Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './styles.scss';
 
 function App() {
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   );
 }
