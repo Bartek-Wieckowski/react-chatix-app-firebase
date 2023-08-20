09beffc14d1fb5f6e1d125f361f775d2b3f40377
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-20 12:36:58 +0200

    implemented search bar to find user

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,60 @@
+import { useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+
 function Search() {
+  const [searchUsername, setSearchUsername] = useState('');
+  const [findUser, setFindUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  async function handleSearch() {
+    const q = query(
+      collection(db, 'users'),
+      where('displayName', '==', searchUsername)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach((doc) => {
+          setFindUser(doc.data());
+          setErr(false);
+        });
+      } else {
+        setFindUser(null);
+        setErr(true);
+      }
+    } catch (error) {
+      setErr(true);
+    }
+  }
+
+  function handleKey(e) {
+    if (e.code === 'Enter') {
+      handleSearch();
+    }
+  }
+
   return (
     <div className="search">
       <div className="search-form">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="user-chat">
-        <img
-          src="https://images.pexels.com/photos/1181424/pexels-photo-1181424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setSearchUsername(e.target.value)}
+          onKeyDown={handleKey}
         />
-        <div className="user-chat-info">
-          <span>Jane</span>
-        </div>
       </div>
+
+      {err && <span className="errMsg">User not found!</span>}
+      {findUser && (
+        <div className="user-chat">
+          <img src={findUser.photoURL} alt="" />
+          <div className="user-chat-info">
+            <span>{findUser.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
