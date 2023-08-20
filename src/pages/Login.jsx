fdc6f8245a41ec4d997f3768ff90e4054247e1d3
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-20 05:37:02 +0200

    created handleSubmit function, used in onSubmit and added error msg if exist

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
 function Login() {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (error) {
+      setErr(true);
+    }
+  }
+
   return (
     <div className="form-container">
       <div className="form-wrapper">
         <span className="logo">CHATix</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="Pass your email" />
           <input type="password" placeholder="Write your password" />
           <button>Login</button>
         </form>
-        <small>You don't have an account? Register</small>
+        {err && <span className="errMsg">Something went wrong!</span>}
+        <small>
+          You don't have an account? <Link to="/register">Register</Link>
+        </small>
       </div>
     </div>
   );
