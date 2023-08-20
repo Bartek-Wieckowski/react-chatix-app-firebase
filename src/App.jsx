d621016ff64258cb111bd166d69d2c8e2b432bd8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-20 05:21:17 +0200

    created ProtectedRoute and used in routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import './styles.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 function App() {
   const { currentUser } = useAuth();
   console.log(currentUser);
+
+  function ProtectedRoute({ children }) {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  }
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />}></Route>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          ></Route>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
