207ffb56c1bfd41cd054b09c9ac4fe3af9359c10
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-21 12:17:53 +0200

    fetch messages if exists and display in template

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
 import Message from './Message';
+import { useChat } from '../context/ChatContext';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+
 function Messages() {
+  const [messages, setMessages] = useState([]);
+  const { data } = useChat();
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, 'chats', data.chatID), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatID]);
+
   return (
     <div className="messages">
-      <Message />
+      {messages.map((msg) => (
+        <Message message={msg} key={msg.id} />
+      ))}
     </div>
   );
 }
